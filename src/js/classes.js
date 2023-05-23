6012c2ef1f339626176dba886b1a06ffd6839f3c
Santiago Pintos <spintos@outlook.es>
2023-05-23 13:34:47 -0300

    Creacion de clases Usuario y Censista
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

---
+++ src/js/classes.js
@@ -0,0 +1,24 @@
+class Persona {
+    constructor(){
+     this.usuarioPersona = {
+        nombre: new String,
+        edad: new Number,
+        ci: new Number,
+        departamento : new String,
+        ocupacion: new String,
+        idCensita: new Number,
+        censado: false,
+     };   
+    }
+}
+
+class Censista {
+    constructor(){
+        this.usuarioCensista = {
+            nombre: new String,
+            usuario: new String,
+            contraseña: new String,
+            idCensista: new Number,
+        }
+    }
+}
