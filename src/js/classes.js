2ed0d9fa298b28c22cc543e70ac5839fadf2efd0
Santiago Pintos <spintos@outlook.es>
2023-06-16 17:05:24 -0300

    Introducción de método cantPersonasNoTrabajanPorDepartamento

--- src/js/classes.js
+++ src/js/classes.js
@@ -776,6 +776,43 @@ class App {
 
         return trabajadores;
     }
+
+    /* 
+        Método que retorna objeto con la cantidad de personas que NO trabajan en cada departamento
+        (Tiene en cuenta censos validados y no validados).    
+    */
+    cantPersonasNoTrabajanPorDepartamento(){
+        let noTrabajan = {
+            1: 0,
+            2: 0,
+            3: 0,
+            4: 0,
+            5: 0,
+            6: 0,
+            7: 0,
+            8: 0,
+            9: 0,
+            10: 0,
+            11: 0,
+            12: 0,
+            13: 0,
+            14: 0,
+            15: 0,
+            16: 0,
+            17: 0,
+            18: 0,
+            19: 0,
+        };
+
+        for (let i = 0; i < this.baseDeDatosCensos.length; i++) {
+            const censo = this.baseDeDatosCensos[i];
+            if (censo.ocupacion==4) {
+                noTrabajan[censo.departamento] = noTrabajan[censo.departamento]+1; 
+            }
+        }
+
+        return noTrabajan;
+    }
 }
 
 class Censo {
